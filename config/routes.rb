88d37841625e5cba68a654e68a7a31de56c5dca5
Nakamura Kosuke <ncl0709@gmail.com>
2019-02-09 19:12:49 +0900

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Samplar::Engine.routes.draw do
   get  '/',                      to: 'samplar#index'
-  get  '/:client/:method',       to: 'samplar#show'
-  post '/:client/:method',       to: 'samplar#create'
-  get  '/:client/self/:method',  to: 'samplar_klass#show'
-  post '/:client/self/:method/', to: 'samplar_klass#create'
+  get  '/:client/:method',       to: 'instance_methods#show'
+  post '/:client/:method',       to: 'instance_methods#create'
+  get  '/:client/self/:method',  to: 'class_methods#show'
+  post '/:client/self/:method/', to: 'class_methods#create'
 end
