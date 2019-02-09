fe00e54ec5a2104e440bfee60567e0fe790c499c
Nakamura Kosuke <ncl0709@gmail.com>
2019-02-09 11:39:37 +0900

    Delete

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,4 @@
 require 'spec_helper'
-# ENV['RAILS_ENV'] ||= 'test'
-# require File.expand_path('./dummy/config/environment', __FILE__)
-# abort("The Rails environment is running in production mode!") if Rails.env.production?
-# require 'rspec/rails'
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
