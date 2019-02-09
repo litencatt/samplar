15de6f04cb2491ef92d1903d94224c4e649a47f5
Nakamura Kosuke <ncl0709@gmail.com>
2019-02-09 11:40:00 +0900

    Samplar view is slim

--- samplar.gemspec
+++ samplar.gemspec
@@ -21,4 +21,5 @@ Gem::Specification.new do |s|
 
   s.add_development_dependency "sqlite3", "~> 1.3.6"
   s.add_development_dependency "rspec-rails"
+  s.add_development_dependency "slim-rails"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 ENV['RAILS_ENV'] ||= 'test'
+
 require File.expand_path('../dummy/config/environment.rb', __FILE__)
 require 'rspec/rails'
+require 'slim'
 
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
