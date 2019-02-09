b354c9c4a8db21b8f1f7438882eaf516ca45b730
Nakamura Kosuke <ncl0709@gmail.com>
2019-02-09 11:40:38 +0900

    Simplify

--- spec/controllers/samplar_controller_spec.rb
+++ spec/controllers/samplar_controller_spec.rb
@@ -1,9 +1,9 @@
 describe Samplar::SamplarController, type: :controller do
   routes { Samplar::Engine.routes }
+
   describe "GET /samplar" do
-    it "works! (now write some real specs)" do
-      get :index
-      expect(response).to have_http_status(200)
-    end
+    before { get :index }
+
+    it { expect(response).to have_http_status(200) }
   end
 end
