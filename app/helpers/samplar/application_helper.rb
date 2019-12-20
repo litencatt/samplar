b2d9ea49d7328f4d774a5e21b9322d0ce385745d
litencatt <ncl0709@gmail.com>
2019-12-20 17:25:15 +0900

    classify convert name to singular

--- app/helpers/samplar/application_helper.rb
+++ app/helpers/samplar/application_helper.rb
@@ -1,7 +1,7 @@
 module Samplar
   module ApplicationHelper
     def classified_name(name)
-        name.split('-').map(&:classify).join('::')
+      name.split('-').map(&:titleize).join('::')
     end
   end
 end
